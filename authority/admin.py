dac56480d1ea9b0c03800b0675c80ec7975806ea
Miguel Vieira <jmvieira@gmail.com>
2018-11-05 16:37:40 +0000

    Application to model entity authority descriptions. Work in progress.

---
+++ authority/admin.py
@@ -0,0 +1,172 @@
+import nested_admin
+from authority.models import (BiographyHistory, Description, Entity,
+                              EntityType, Event, FamilyTreeLevel, Function,
+                              Identity, LanguageScript, LegalStatus,
+                              LocalDescription, Mandate, NameEntry, NamePart,
+                              NamePartType, Place, Structure)
+from ckeditor.widgets import CKEditorWidget
+from django.contrib import admin
+from django.db import models
+from reversion.admin import VersionAdmin
+
+
+class EventInline(nested_admin.NestedTabularInline):
+    model = Event
+
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class BiographyHistoryInline(nested_admin.NestedStackedInline):
+    model = BiographyHistory
+
+    extra = 1
+    inlines = [EventInline]
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class LanguageScriptInline(nested_admin.NestedTabularInline):
+    model = LanguageScript
+
+    autocomplete_fields = ['language', 'script']
+    extra = 1
+
+
+class LegalStatusInline(nested_admin.NestedStackedInline):
+    model = LegalStatus
+
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class LocalDescriptionInline(nested_admin.NestedStackedInline):
+    model = LocalDescription
+
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class MandateInline(nested_admin.NestedStackedInline):
+    model = Mandate
+
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class PlaceInline(nested_admin.NestedStackedInline):
+    model = Place
+
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+class StructureInline(nested_admin.NestedTabularInline):
+    model = Structure
+
+    autocomplete_fields = ['level']
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+@admin.register(Description)
+class DescriptionAdmin(nested_admin.NestedModelAdmin, VersionAdmin):
+    autocomplete_fields = ['function']
+    inlines = [PlaceInline, LanguageScriptInline, BiographyHistoryInline,
+               LocalDescriptionInline, MandateInline, LegalStatusInline,
+               StructureInline]
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+    list_display = ['entity', 'date_from', 'date_to']
+
+
+@admin.register(EntityType)
+class EntityTypeAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(FamilyTreeLevel)
+class FamilyTreeLevelAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Function)
+class FunctionAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+class NamePartInline(nested_admin.NestedTabularInline):
+    model = NamePart
+
+    autocomplete_fields = ['name_part_type']
+    extra = 2
+
+
+class NameEntryInline(nested_admin.NestedTabularInline):
+    model = NameEntry
+
+    autocomplete_fields = ['language', 'script']
+    extra = 1
+    inlines = [NamePartInline]
+
+
+@admin.register(Identity)
+class IdentityAdmin(nested_admin.NestedModelAdmin, VersionAdmin):
+    # autocomplete_fields = ['entity']
+    inlines = [NameEntryInline]
+    list_display = ['entity', 'authorised_form', 'date_from', 'date_to']
+
+
+# @admin.register(NameEntry)
+# class NameEntryAdmin(VersionAdmin):
+#     inlines = [NamePartInline]
+#     list_display = ['identity', 'authorised_form', 'language', 'script']
+
+
+# @admin.register(NamePart)
+# class NamePartAdmin(VersionAdmin):
+#     list_display = ['name_entry', 'name_part_type', 'part']
+
+
+class DescriptionInline(nested_admin.NestedTabularInline):
+    model = Description
+
+    autocomplete_fields = DescriptionAdmin.autocomplete_fields
+    extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+    inlines = DescriptionAdmin.inlines
+
+
+class IdentityInline(nested_admin.NestedTabularInline):
+    model = Identity
+
+    extra = 1
+    inlines = [NameEntryInline]
+
+
+@admin.register(Entity)
+class Entity(nested_admin.NestedModelAdmin, VersionAdmin):
+    autocomplete_fields = ['entity_type']
+    inlines = [IdentityInline, DescriptionInline]
+    list_display = ['entity_type']
+
+
+@admin.register(NamePartType)
+class NamePartTypeAdmin(admin.ModelAdmin):
+    search_fields = ['title']
---
+++ authority/models.py
@@ -0,0 +1,185 @@
+import reversion
+from django.db import models
+from languages_plus.models import Language
+from model_utils.models import TimeStampedModel
+from script_codes.models import Script
+
+
+class DateRangeMixin(models.Model):
+    date_from = models.DateField()
+    date_to = models.DateField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class LanguageScriptMixin(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    script = models.ForeignKey(Script, on_delete=models.PROTECT)
+
+    class Meta:
+        abstract = True
+
+
+class EntityType(TimeStampedModel):
+    title = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+@reversion.register()
+class Entity(TimeStampedModel):
+    entity_type = models.ForeignKey(EntityType, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Entities'
+
+
+@reversion.register()
+class Identity(DateRangeMixin, TimeStampedModel):
+    entity = models.ForeignKey(
+        Entity, on_delete=models.CASCADE, related_name='identities')
+
+    class Meta:
+        verbose_name_plural = 'Identities'
+
+    @property
+    def authorised_form(self):
+        return self.name_entries.filter(authorised_form=True).first()
+
+
+@reversion.register()
+class NameEntry(DateRangeMixin, LanguageScriptMixin, TimeStampedModel):
+    identity = models.ForeignKey(
+        Identity, on_delete=models.CASCADE, related_name='name_entries')
+
+    authorised_form = models.BooleanField()
+
+    class Meta:
+        verbose_name_plural = 'Name entries'
+
+    def __str__(self):
+        return ', '.join(['{}: {}'.format(
+            p.name_part_type, p.part) for p in self.parts])
+
+
+class NamePartType(TimeStampedModel):
+    title = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+@reversion.register()
+class NamePart(TimeStampedModel):
+    name_entry = models.ForeignKey(
+        NameEntry, on_delete=models.CASCADE, related_name='parts')
+    name_part_type = models.ForeignKey(NamePartType, on_delete=models.PROTECT)
+    part = models.CharField(max_length=256)
+
+    def __str__(self):
+        return '{}: {}'.format(self.name_part_type, self.part)
+
+
+class Function(TimeStampedModel):
+    title = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+@reversion.register()
+class Description(DateRangeMixin, TimeStampedModel):
+    entity = models.ForeignKey(
+        Entity, on_delete=models.CASCADE, related_name='descriptions')
+
+    function = models.ManyToManyField(Function)
+
+    structure_or_genealogy = models.TextField(blank=True, null=True)
+
+
+@reversion.register()
+class Place(DateRangeMixin, TimeStampedModel):
+    description = models.ForeignKey(
+        Description, on_delete=models.CASCADE, related_name='places')
+
+    name = models.CharField(max_length=256)
+    address = models.TextField(blank=True, null=True)
+    role = models.TextField(blank=True, null=True)
+
+
+@reversion.register()
+class LanguageScript(LanguageScriptMixin, TimeStampedModel):
+    description = models.ForeignKey(Description, on_delete=models.CASCADE,
+                                    related_name='languages_scripts')
+
+
+@reversion.register()
+class BiographyHistory(TimeStampedModel):
+    description = models.OneToOneField(Description, on_delete=models.CASCADE)
+
+    abstract = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    sources = models.TextField(blank=True, null=True)
+    copyright = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Biography/History'
+        verbose_name_plural = 'Biography/History'
+
+
+@reversion.register()
+class Event(DateRangeMixin, TimeStampedModel):
+    biography_history = models.ForeignKey(
+        BiographyHistory, on_delete=models.CASCADE)
+
+    event = models.TextField()
+    place = models.CharField(max_length=256)
+
+
+@reversion.register()
+class LocalDescription(DateRangeMixin, TimeStampedModel):
+    description = models.ForeignKey(Description, on_delete=models.CASCADE,
+                                    related_name='local_descriptions')
+
+    gender = models.CharField(max_length=256)
+    notes = models.TextField()
+    citation = models.TextField()
+
+
+@reversion.register()
+class Mandate(DateRangeMixin, TimeStampedModel):
+    description = models.ForeignKey(Description, on_delete=models.CASCADE,
+                                    related_name='mandates')
+
+    term = models.CharField(max_length=256, blank=True)
+    notes = models.TextField(blank=True, null=True)
+    citation = models.TextField(blank=True, null=True)
+
+
+@reversion.register()
+class LegalStatus(DateRangeMixin, TimeStampedModel):
+    description = models.ForeignKey(Description, on_delete=models.CASCADE,
+                                    related_name='legal_statuses')
+
+    term = models.CharField(max_length=256, blank=True)
+    notes = models.TextField(blank=True, null=True)
+    citation = models.TextField(blank=True, null=True)
+
+
+class FamilyTreeLevel(TimeStampedModel):
+    title = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+@reversion.register()
+class Structure(TimeStampedModel):
+    description = models.ForeignKey(Description, on_delete=models.CASCADE,
+                                    related_name='structures')
+
+    level = models.ForeignKey(FamilyTreeLevel, on_delete=models.PROTECT)
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    citation = models.TextField(blank=True, null=True)
