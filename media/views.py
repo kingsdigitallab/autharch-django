549414fcd658ee9f1325e84048edf85e0e13752d
Miguel Vieira <jmvieira@gmail.com>
2019-04-10 15:49:10 +0100

    Add missing rest urls and views.

---
+++ media/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+
+from .models import Media
+from .serializers import MediaPolymorphicSerializer
+
+
+class ArchivalRecordViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint to list or view media objects.
+
+    list:
+    Return a list of all the media objects. By default the archival records are
+    ordered by most recently modified.
+
+    read:
+    Return the given media object.
+    """
+    queryset = Media.objects.order_by('modified')
+    serializer_class = MediaPolymorphicSerializer
